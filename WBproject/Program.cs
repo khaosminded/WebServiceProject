a6c4ad7c803541ca6bac376ee891cb5271e3ea75
hanxinlei <khaosminded@vip.qq.com>
2018-03-06 20:58:57 -0800

    + works well on  docker : but docker isn't required anymore. simply install .net core apk on Ubuntu. : whenever need to scale up

--- WBproject/Program.cs
+++ WBproject/Program.cs
@@ -64,6 +64,6 @@ namespace WBproject
             }
         }
         public static IWebHost BuildWebHost(string[] args) =>             WebHost.CreateDefaultBuilder(args)
-                   .UseUrls("http://*:8080/")                 .UseStartup<Startup>()                 .Build(); 
+                   .UseUrls("https://*:8080/")                 .UseStartup<Startup>()                 .Build(); 
     }
 }
